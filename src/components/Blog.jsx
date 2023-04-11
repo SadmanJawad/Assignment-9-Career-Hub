a947aa63880fd47a01e727caca5b225ffbbdce62
SadmanJawad <jawadnabil977@gmail.com>
2023-04-11 06:48:11 +0600

    blog route done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div className='px-96 my-24 text-start border border-blue-950 '>
-            <h1 className='bg-slate-400'>Q-1 : When should we use Context API?</h1>
+        <div className='px-96 my-24 text-start'>
+            <h1 className='bg-slate-400 text-xl font-semibold'>Q-1 : When should we use Context API?</h1>
             <p className='bg-slate-400'>Ans: In normal react application data is passed from top to down (parent to child) via props. But Context API provides a way to share or state between multiple components without prop drilling .</p>
-            <h1 className='bg-green-400 mt-5'>Q-2 : What is custom hook?</h1>
+            <h1 className='bg-green-400 mt-5 text-xl font-semibold'>Q-2 : What is custom hook?</h1>
             <p  className='bg-green-400'>Ans: In React, custom hook is a reusable function that can use to add special and unique function to use react application.If anyone have multiple react hooks that will be used at multiple location in a code , one should use custom React hooks.This helps making the code more readable and make the code clean.</p>
-            <h1 className='bg-red-400 mt-5'>What is useRef()?When should we use this?</h1>
+            <h1 className='bg-red-400 mt-5 text-xl font-semibold'>What is useRef()?When should we use this?</h1>
             <p className='bg-red-400'>Ans: In React, useRef is a hook that allows you to create a mutable reference that can be attached to a DOM element or any other value and persist across renders without causing the component to re-render. It enables you to directly access and modify DOM elements, as well as store values that do not require re-rendering when updated.</p>
-            <h1 className='bg-yellow-300 mt-5'>What is useMemo()?When should we use this?</h1>
-            <p className='bg-yellow-300'>Ans: </p>
+            <h1 className='bg-yellow-300 mt-5 text-xl font-semibold'>What is useMemo()?When should we use this?</h1>
+            <p className='bg-yellow-300'>Ans: useMemo is actually a hook which  memorize computation which is valuable and over the odds.It is also avoid unnecessary re-renders.</p>
         </div>
     );
 };
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,77 @@
 import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+    const assignmentMarksArr = [
+        {
+                id: 1,
+                a_serial:1,
+                a_name: "HTML",
+                a_nmb: 50
+        },
+        {
+                id: 2,
+                a_serial:2,
+                a_name: "CSS",
+                a_nmb: 59
+        },
+        {
+                id: 3,
+                a_serial:3,
+                a_name: "CSS Framework",
+                a_nmb: 60
+        },
+        {
+                id: 4,
+                a_serial:4,
+                a_name: "JS Basic",
+                a_nmb: 45
+        },
+        {
+                id: 5,
+                a_serial:5,
+                a_name: "DOM JS",
+                a_nmb: 60
+        },
+        {
+                id: 6,
+                a_serial:6,
+                a_name: "ES6 & API",
+                a_nmb: 59
+        },
+        {
+                id: 7,
+                a_serial:7,
+                a_name: "Debug",
+                a_nmb: 60
+        },
+        {
+            id: 8,
+            a_serial:8,
+            a_name: "React Basic",
+            a_nmb: 60
+    }
+    ]
+
     return (
-        <div>
-            This is statistics
+        <div  width="100%" height="100%"> 
+        <AreaChart   width={500}
+          height={400}
+          data={assignmentMarksArr}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+          >
+             <CartesianGrid strokeDasharray="3 3" />
+             <XAxis dataKey="a_name" />
+          <YAxis dataKey=""/>
+          <Tooltip />
+          <Area type="monotone" dataKey="a_nmb" stroke="#8884d8" fill="#8884d8"   activeDot={{ r: 8 }}/>
+        </AreaChart>
         </div>
     );
 };
